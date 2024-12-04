7e3c77c17f92534db60713392788d957b42122a2
Raaid Tanveer <raaidraiyantanveer@gmail.com>
2024-12-03 23:12:31 -0800

    cargo

--- src/day/mod.rs
+++ src/day/mod.rs
@@ -1,133 +1,50 @@
 // This file is auto-generated. Please edit build.rs to make changes
-use super::stage::Stage;
 use std::error::Error;
-mod day01;
-pub mod day1 {
-    pub use super::day01::run;
-}
-mod day02;
-pub mod day2 {
-    pub use super::day02::run;
-}
-mod day03;
-pub mod day3 {
-    pub use super::day03::run;
-}
-mod day04;
-pub mod day4 {
-    pub use super::day04::run;
-}
-use std::fmt;
-#[derive(Debug)]
-struct DayError(u8);
-impl fmt::Display for DayError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Invalid day: {}", self.0)
-    }
-}
-impl Error for DayError {}
-pub mod day5 {
-    pub fn run(_s: &str, _stage: super::Stage) -> Result<String, Box<dyn super::Error>> {
-        Err(Box::new(super::DayError(5.into())))
-    }
-}
-pub mod day6 {
-    pub fn run(_s: &str, _stage: super::Stage) -> Result<String, Box<dyn super::Error>> {
-        Err(Box::new(super::DayError(6.into())))
-    }
-}
-pub mod day7 {
-    pub fn run(_s: &str, _stage: super::Stage) -> Result<String, Box<dyn super::Error>> {
-        Err(Box::new(super::DayError(7.into())))
-    }
-}
-pub mod day8 {
-    pub fn run(_s: &str, _stage: super::Stage) -> Result<String, Box<dyn super::Error>> {
-        Err(Box::new(super::DayError(8.into())))
-    }
-}
-pub mod day9 {
-    pub fn run(_s: &str, _stage: super::Stage) -> Result<String, Box<dyn super::Error>> {
-        Err(Box::new(super::DayError(9.into())))
-    }
-}
-pub mod day10 {
-    pub fn run(_s: &str, _stage: super::Stage) -> Result<String, Box<dyn super::Error>> {
-        Err(Box::new(super::DayError(10.into())))
-    }
-}
-pub mod day11 {
-    pub fn run(_s: &str, _stage: super::Stage) -> Result<String, Box<dyn super::Error>> {
-        Err(Box::new(super::DayError(11.into())))
-    }
-}
-pub mod day12 {
-    pub fn run(_s: &str, _stage: super::Stage) -> Result<String, Box<dyn super::Error>> {
-        Err(Box::new(super::DayError(12.into())))
-    }
-}
-pub mod day13 {
-    pub fn run(_s: &str, _stage: super::Stage) -> Result<String, Box<dyn super::Error>> {
-        Err(Box::new(super::DayError(13.into())))
-    }
-}
-pub mod day14 {
-    pub fn run(_s: &str, _stage: super::Stage) -> Result<String, Box<dyn super::Error>> {
-        Err(Box::new(super::DayError(14.into())))
-    }
-}
-pub mod day15 {
-    pub fn run(_s: &str, _stage: super::Stage) -> Result<String, Box<dyn super::Error>> {
-        Err(Box::new(super::DayError(15.into())))
-    }
-}
-pub mod day16 {
-    pub fn run(_s: &str, _stage: super::Stage) -> Result<String, Box<dyn super::Error>> {
-        Err(Box::new(super::DayError(16.into())))
-    }
-}
-pub mod day17 {
-    pub fn run(_s: &str, _stage: super::Stage) -> Result<String, Box<dyn super::Error>> {
-        Err(Box::new(super::DayError(17.into())))
-    }
-}
-pub mod day18 {
-    pub fn run(_s: &str, _stage: super::Stage) -> Result<String, Box<dyn super::Error>> {
-        Err(Box::new(super::DayError(18.into())))
-    }
-}
-pub mod day19 {
-    pub fn run(_s: &str, _stage: super::Stage) -> Result<String, Box<dyn super::Error>> {
-        Err(Box::new(super::DayError(19.into())))
-    }
-}
-pub mod day20 {
-    pub fn run(_s: &str, _stage: super::Stage) -> Result<String, Box<dyn super::Error>> {
-        Err(Box::new(super::DayError(20.into())))
-    }
-}
-pub mod day21 {
-    pub fn run(_s: &str, _stage: super::Stage) -> Result<String, Box<dyn super::Error>> {
-        Err(Box::new(super::DayError(21.into())))
-    }
-}
-pub mod day22 {
-    pub fn run(_s: &str, _stage: super::Stage) -> Result<String, Box<dyn super::Error>> {
-        Err(Box::new(super::DayError(22.into())))
-    }
-}
-pub mod day23 {
-    pub fn run(_s: &str, _stage: super::Stage) -> Result<String, Box<dyn super::Error>> {
-        Err(Box::new(super::DayError(23.into())))
-    }
-}
-pub mod day24 {
-    pub fn run(_s: &str, _stage: super::Stage) -> Result<String, Box<dyn super::Error>> {
-        Err(Box::new(super::DayError(24.into())))
-    }
-}
-pub mod day25 {
-    pub fn run(_s: &str, _stage: super::Stage) -> Result<String, Box<dyn super::Error>> {
-        Err(Box::new(super::DayError(25.into())))
-    }
-}
+use super::stage::Stage;
+mod day01; pub mod day1 {pub use super::day01::run;}
+mod day02; pub mod day2 {pub use super::day02::run;}
+mod day03; pub mod day3 {pub use super::day03::run;}
+mod day04; pub mod day4 {pub use super::day04::run;}
+use std::fmt;#[derive(Debug)]struct DayError(u8);impl fmt::Display for DayError {fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {write!(f, "Invalid day: {}", self.0)}}impl Error for DayError {}
+pub mod day5 {pub fn run(_s: &str, _stage: super::Stage) -> Result<String, Box<dyn super::Error>> {Err(Box::new(super::DayError(5.into())))
+                    }}
+pub mod day6 {pub fn run(_s: &str, _stage: super::Stage) -> Result<String, Box<dyn super::Error>> {Err(Box::new(super::DayError(6.into())))
+                    }}
+pub mod day7 {pub fn run(_s: &str, _stage: super::Stage) -> Result<String, Box<dyn super::Error>> {Err(Box::new(super::DayError(7.into())))
+                    }}
+pub mod day8 {pub fn run(_s: &str, _stage: super::Stage) -> Result<String, Box<dyn super::Error>> {Err(Box::new(super::DayError(8.into())))
+                    }}
+pub mod day9 {pub fn run(_s: &str, _stage: super::Stage) -> Result<String, Box<dyn super::Error>> {Err(Box::new(super::DayError(9.into())))
+                    }}
+pub mod day10 {pub fn run(_s: &str, _stage: super::Stage) -> Result<String, Box<dyn super::Error>> {Err(Box::new(super::DayError(10.into())))
+                    }}
+pub mod day11 {pub fn run(_s: &str, _stage: super::Stage) -> Result<String, Box<dyn super::Error>> {Err(Box::new(super::DayError(11.into())))
+                    }}
+pub mod day12 {pub fn run(_s: &str, _stage: super::Stage) -> Result<String, Box<dyn super::Error>> {Err(Box::new(super::DayError(12.into())))
+                    }}
+pub mod day13 {pub fn run(_s: &str, _stage: super::Stage) -> Result<String, Box<dyn super::Error>> {Err(Box::new(super::DayError(13.into())))
+                    }}
+pub mod day14 {pub fn run(_s: &str, _stage: super::Stage) -> Result<String, Box<dyn super::Error>> {Err(Box::new(super::DayError(14.into())))
+                    }}
+pub mod day15 {pub fn run(_s: &str, _stage: super::Stage) -> Result<String, Box<dyn super::Error>> {Err(Box::new(super::DayError(15.into())))
+                    }}
+pub mod day16 {pub fn run(_s: &str, _stage: super::Stage) -> Result<String, Box<dyn super::Error>> {Err(Box::new(super::DayError(16.into())))
+                    }}
+pub mod day17 {pub fn run(_s: &str, _stage: super::Stage) -> Result<String, Box<dyn super::Error>> {Err(Box::new(super::DayError(17.into())))
+                    }}
+pub mod day18 {pub fn run(_s: &str, _stage: super::Stage) -> Result<String, Box<dyn super::Error>> {Err(Box::new(super::DayError(18.into())))
+                    }}
+pub mod day19 {pub fn run(_s: &str, _stage: super::Stage) -> Result<String, Box<dyn super::Error>> {Err(Box::new(super::DayError(19.into())))
+                    }}
+pub mod day20 {pub fn run(_s: &str, _stage: super::Stage) -> Result<String, Box<dyn super::Error>> {Err(Box::new(super::DayError(20.into())))
+                    }}
+pub mod day21 {pub fn run(_s: &str, _stage: super::Stage) -> Result<String, Box<dyn super::Error>> {Err(Box::new(super::DayError(21.into())))
+                    }}
+pub mod day22 {pub fn run(_s: &str, _stage: super::Stage) -> Result<String, Box<dyn super::Error>> {Err(Box::new(super::DayError(22.into())))
+                    }}
+pub mod day23 {pub fn run(_s: &str, _stage: super::Stage) -> Result<String, Box<dyn super::Error>> {Err(Box::new(super::DayError(23.into())))
+                    }}
+pub mod day24 {pub fn run(_s: &str, _stage: super::Stage) -> Result<String, Box<dyn super::Error>> {Err(Box::new(super::DayError(24.into())))
+                    }}
+pub mod day25 {pub fn run(_s: &str, _stage: super::Stage) -> Result<String, Box<dyn super::Error>> {Err(Box::new(super::DayError(25.into())))
+                    }}
