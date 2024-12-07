0e85f973bde2382763868f5a7c6f0520dc284962
Raaid Tanveer <raaidraiyantanveer@gmail.com>
2024-12-06 21:20:44 -0800

    Day 7

---
+++ src/day/day07.rs
@@ -0,0 +1,103 @@
+use super::super::stage::{Stage, StageUnimplemented};
+use rayon::prelude::*;
+pub use std::error::Error;
+
+fn parse(s: &str) -> Vec<(u128, Vec<u128>)> {
+    s.lines()
+        .into_iter()
+        .map(|line| {
+            let result: Vec<&str> = line.trim().split(": ").collect();
+            let lhs: u128 = result[0].trim().parse().unwrap();
+            let rhs: Vec<u128> = result[1]
+                .split(" ")
+                .map(|x| x.trim().parse().unwrap())
+                .collect();
+            (lhs, rhs)
+        })
+        .collect()
+}
+
+fn solve(lhs: u128, rhs_left: u128, rhs_right: &[u128]) -> bool {
+    if rhs_right.len() == 0 {
+        lhs == rhs_left
+    } else {
+        solve(lhs, rhs_left + rhs_right[0], &rhs_right[1..])
+            || solve(lhs, rhs_left * rhs_right[0], &rhs_right[1..])
+    }
+}
+
+fn num_digits(x: u128) -> u128 {
+    if x == 0 {
+        0
+    } else {
+        1 + num_digits(x / 10)
+    }
+}
+
+// x^2
+fn pow(x: u128, y: u128) -> u128 {
+    if y == 0 {
+        1
+    } else {
+        x * pow(x, y - 1)
+    }
+}
+
+fn concat(x: u128, y: u128) -> u128 {
+    x * pow(10, num_digits(y)) + y
+}
+
+fn solve2(lhs: u128, rhs_left: u128, rhs_right: &[u128]) -> bool {
+    if rhs_right.len() == 0 {
+        lhs == rhs_left
+    } else {
+        solve2(lhs, rhs_left + rhs_right[0], &rhs_right[1..])
+            || solve2(lhs, rhs_left * rhs_right[0], &rhs_right[1..])
+            || solve2(lhs, concat(rhs_left, rhs_right[0]), &rhs_right[1..])
+    }
+}
+
+pub fn run(s: &str, stage: Stage) -> Result<String, Box<dyn Error>> {
+    match stage {
+        Stage::A => {
+            let eqns = parse(s);
+            let result = eqns
+                .par_iter()
+                .map(|(lhs, rhs)| {
+                    (if rhs.len() == 0 {
+                        if *lhs == 0 {
+                            1
+                        } else {
+                            0
+                        }
+                    } else if solve(*lhs, rhs[0], &rhs[1..]) {
+                        1
+                    } else {
+                        0
+                    }) * (*lhs)
+                })
+                .reduce(|| 0, |x, y| x + y);
+            Ok(result.to_string())
+        }
+        Stage::B => {
+            let eqns = parse(s);
+            let result = eqns
+                .par_iter()
+                .map(|(lhs, rhs)| {
+                    (if rhs.len() == 0 {
+                        if *lhs == 0 {
+                            1
+                        } else {
+                            0
+                        }
+                    } else if solve2(*lhs, rhs[0], &rhs[1..]) {
+                        1
+                    } else {
+                        0
+                    }) * (*lhs)
+                })
+                .reduce(|| 0, |x, y| x + y);
+            Ok(result.to_string())
+        }
+    }
+}
